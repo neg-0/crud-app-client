d3d1ebecb2fbb088be2da4e3dcb2a58690d10039
Dustin Stringer <dustin.stringer@us.af.mil>
2023-06-25 22:03:56 -0400

    added mui data grid view for inventory items

--- src/components/InventoryList.tsx
+++ src/components/InventoryList.tsx
@@ -1,24 +1,58 @@
+import { Button, Stack, Switch, Typography } from '@mui/material';
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import axios from 'axios';
 import { useEffect, useState } from "react";
-import InventoryItem, { InventoryItemData } from "./InventoryItem";
+import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuthentication';
+
+type InventoryItemData = {
+  id: number,
+  item_name: string,
+  description: string,
+  quantity: number,
+  user_id: number
+}
+
+const inventoryItemColumns: GridColDef[] = [
+  { field: 'item_name', headerName: 'Name', width: 300, renderCell: (params: GridRenderCellParams<InventoryItemData>) => (<Link to={`/viewItem/${params.row.id}`}>{params.value}</Link>) },
+  { field: 'description', headerName: 'Description', width: 800 },
+  { field: 'quantity', headerName: 'Quantity', width: 100 },
+  { field: 'username', headerName: 'Owner Name', width: 200, valueGetter: (params) => `${params.row.user?.first_name} ${params.row.user?.last_name}` }
+]
 
 export default function InventoryList() {
   const [inventoryItems, setInventoryItems] = useState<InventoryItemData[] | null>([]);
+  const [displayAllItems, setDisplayAllItems] = useState(true);
+
+  const { user } = useAuth();
 
   useEffect(() => {
-    axios.get('http://localhost:3000/items')
-    .then(res => {
-      console.log(res)
-      return res.data;
-    })
-    .then(data => setInventoryItems(data as InventoryItemData[]))
-  }, [])
+    // Build the url based on display options
+    const url = `http://localhost:3000/items?descLimit=100&addUserData=true&onlyMyItems=${!displayAllItems}`;
+
+    axios.get(url)
+      .then(res => {
+        console.log(res)
+        return res.data;
+      })
+      .then(data => setInventoryItems(data as InventoryItemData[]))
+  }, [displayAllItems, user])
 
   if (!inventoryItems) return null;
   return (
     <>
-    {inventoryItems.map((item, index) => 
-      <InventoryItem key={index} item={item}></InventoryItem>)
-    }</>)
-  
+      {user && (
+        <Button variant="outlined" color="primary" onClick={() => setDisplayAllItems(!displayAllItems)} >
+          <Stack spacing={2} direction="row" alignItems="center">
+            <Typography>Display My Items</Typography>
+            <Switch checked={displayAllItems} />
+            <Typography>Display All Items</Typography>
+          </Stack>
+        </Button>)}
+      <DataGrid rows={inventoryItems} columns={inventoryItemColumns}
+        columnVisibilityModel={{
+          username: (user !== null)
+        }} />
+    </>)
+
 }
--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,9 +1,11 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import InventoryList from "../components/InventoryList";
 
 export default function Inventory() {
   return (<>
     <Typography variant="h1">Inventory</Typography>
-    <InventoryList />
+    <Box sx={{ height: '75vh', width: 'calc(100% - 16px)' }}>
+      <InventoryList />
+    </Box>
   </>)
 }
